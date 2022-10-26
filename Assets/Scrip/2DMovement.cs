a4a7c8386593b2e7a75d5d6e6846b2cafdcc2097
Sean <seangiesselink@gmail.com>
2022-10-26 16:08:08 +0200

    Converted project into 2D mode

---
+++ Assets/Scrip/2DMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 2DMovement : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
