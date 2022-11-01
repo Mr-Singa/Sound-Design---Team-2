53a59596ad78929a4fb4f6af31f67df2ead96fc9
Koekiemonster69 <74185498+Koekiemonster69@users.noreply.github.com>
2022-11-01 17:05:45 +0100

    Implemented sprinting + panting
    and added an avalanche sound

--- Assets/Scrip/PlaneMovement.cs
+++ Assets/Scrip/PlaneMovement.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class PlaneMovement : MonoBehaviour
 {
-    public float SPEED;
+    public float SPEED = 0;
+    public float WALKSPEED;
+    public float SPRINTSPEED;
+
+    public MoveMode moveMode = MoveMode.Walking;
+
+    public float maxSprintDuration;
+    float sprintingTime = 0;
+    public float pantingDuration;
+    float pantingTime = 0;
 
     public AudioSource footstepsNormal;
     public AudioSource footstepsWet;
+    public AudioSource sprinting;
     public AudioSource panting;
 
     void Update()
@@ -18,23 +28,77 @@ public class PlaneMovement : MonoBehaviour
 
     public void Movement()
     {
+        switch (moveMode)
+        {
+            case MoveMode.Walking:
+                SPEED = WALKSPEED;
+                panting.enabled = false;
+                sprinting.enabled = false;
+
+                if (sprintingTime > 0)
+                    sprintingTime -= Time.deltaTime;
+
+                if (Input.GetKey(KeyCode.LeftShift))
+                    moveMode = MoveMode.Sprinting;
+
+                break;
+
+            case MoveMode.Sprinting:
+                SPEED = SPRINTSPEED;
+                sprintingTime += Time.deltaTime;
+                panting.enabled = false;
+                footstepsNormal.enabled = false;
+
+                if (sprintingTime > maxSprintDuration)
+                {
+                    pantingTime = 0;
+                    moveMode = MoveMode.Panting;
+                }
+
+
+                if (!Input.GetKey(KeyCode.LeftShift))
+                    moveMode = MoveMode.Walking;
+                
+                break;
+
+            case MoveMode.Panting:
+                SPEED = WALKSPEED;
+                pantingTime += Time.deltaTime;
+                panting.enabled = true;
+                sprinting.enabled = false;
+
+                if (pantingTime > pantingDuration)
+                {
+                    sprintingTime = 0;
+                    moveMode = MoveMode.Walking;
+                    
+                }
+
+                break;
+        }
+        
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += transform.up * SPEED * Time.deltaTime;
-            footstepsNormal.enabled = true;
-        }
-        else
-        {
-            footstepsNormal.enabled = false;
-        }
 
-        if (Input.GetKey(KeyCode.S))
+            if (moveMode == MoveMode.Sprinting)
+                sprinting.enabled = true;
+            else
+                footstepsNormal.enabled = true;
+            
+        }
+        else if (Input.GetKey(KeyCode.S))
         {
             transform.position += transform.up * -SPEED * Time.deltaTime;
-            footstepsNormal.enabled = true;
+            
+            if (moveMode == MoveMode.Sprinting)
+                sprinting.enabled = true;
+            else
+                footstepsNormal.enabled = true;
         }
         else
         {
+            sprinting.enabled = false;
             footstepsNormal.enabled = false;
         }
     }
@@ -50,5 +114,12 @@ public class PlaneMovement : MonoBehaviour
         {
             transform.Rotate(0, 0, -SPEED * 10 * Time.deltaTime);
         }
+    }
+
+    public enum MoveMode
+    {
+        Walking,
+        Sprinting,
+        Panting
     }
 }
